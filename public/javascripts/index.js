831d6bf4c69f3233ab8897f8573353e41a608da3
Sapan Purohit <Sapan_Purohit@ad.infosys.com>
2017-10-06 19:37:38 +0530

    trying grid code14

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -46,7 +46,7 @@ $(document).ready(function () {
                 dataSeries: [{
                     seriesType: "bar",
                     collectionAlias: "Pressure",
-                    data: [obj.pressure[0].Value, 400, 743, 503, 702, 383]
+                    data: [parseInt(obj.pressure[0].Value, 5), 400, 743, 503, 702, 383]
                 }, {
                     seriesType: "bar",
                     collectionAlias: "Temperature",
