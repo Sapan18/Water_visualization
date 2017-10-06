db3810c3e2cf7d5680cfa2b9087fa8744005b63f
Sapan Purohit <Sapan_Purohit@ad.infosys.com>
2017-10-06 17:12:35 +0530

    trying grid code1

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,14 +1,11 @@
 $(document).ready(function () {
 	
 	//variables declaration
-	var gridData=[];
+	var tableData=[], tableDataReverse=[];
 	
 	$("#grid").shieldGrid({
             dataSource: {
-				data: gridData
-			},
-			events: {
-			dataBound: gridDataBound
+				data: tableDataReverse
 			},
 			rowHover: false,			
             scrolling: {
@@ -22,8 +19,52 @@ $(document).ready(function () {
             ]
         });
 		
-	gridData.push({
-		key: "internalTemperature",
-		value: "128"
-	});
+		
+	//updating table (live)
+    function refreshGird() {
+        var grid = $("#grid").swidget(),
+            initialOptions = grid.initialOptions;
+        initialOptions.dataSource = {
+            data: tableDataReverse
+        };
+        grid.refresh(initialOptions);
+    }
+	
+	//styling table (assigning red/green colour)
+	function gridDataBound(e) {
+		var data = e.target.dataSource.view;
+		var rows = e.target.contentTable.find(">tbody>tr");
+		for (var i = 0; i < data.length; i++) {
+            var item = data[i];
+            if (item.message == thresholdMsg) {
+                $(rows[i].cells[3]).addClass("red");
+            }
+            if (item.message == msg) {
+                $(rows[i].cells[3]).addClass("green");
+            }
+        }
+    }
+		
+	if(tableData.length == 5){
+						for(var i=0; i<(tableData.length)-1; i++){
+							tableData[i]= tableData[i+1];
+						}
+						tableData[4]={
+							key: "internalTemperature",
+							value: "128"
+						};
+					}
+					else{
+						tableData.push({
+							key: "internalTemperature",
+							value: "128"
+						});
+					}	
+		tableDataReverse = tableData.slice(0);
+		tableDataReverse.reverse();
+		
+	
+	
+	
+	
 });
