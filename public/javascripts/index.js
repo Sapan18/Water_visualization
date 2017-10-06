c72117e37657160b03bc6c3d8fb7267af3d8c2e6
Sapan Purohit <Sapan_Purohit@ad.infosys.com>
2017-10-06 17:01:54 +0530

    trying grid code

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,40 +1,29 @@
 $(document).ready(function () {
 	
-	$("#chart").shieldChart({
-                theme: "light",
-                exportOptions: {
-                    image: false,
-                    print: false
-                },
-                primaryHeader: {
-                    text: "Pressure, vibration and termperature statistics over six interval"
-                },
-                zoomMode: "xy",
-                seriesSettings: {
-                    bar: {
-                        barOffset: 0
-                    }
-                },
-                axisX: {
-                    categoricalValues: ["1st", "2nd", "3rd", "4th", "5th", "6th"]
-                },
-                axisY: {
-                    title: {
-                        text: "Value statistics"
-                    }
-                },
-                dataSeries: [{
-                    seriesType: "bar",
-                    collectionAlias: "Pressure",
-                    data: [630, 400, 743, 503, 702, 383]
-                }, {
-                    seriesType: "bar",
-                    collectionAlias: "Vibration",
-                    data: [152, 234, 123, 348]
-                }, {
-                    seriesType: "bar",
-                    collectionAlias: "Temperature",
-                    data: [72, 90, 84, 78, 67, 83]
-                }]
-            });
+	//variables declaration
+	var gridData=[];
+	
+	$("#grid").shieldGrid({
+            dataSource: {
+				data: gridData
+			},
+			events: {
+			dataBound: gridDataBound
+			},
+			rowHover: false,			
+            scrolling: {
+                virtual: true
+            },
+            sorting: true,
+            columnReorder: true,
+            columns: [
+                { field: "key", title: "Key" },
+                { field: "value", title: "Value" }
+            ]
+        });
+		
+	gridData.push({
+		key: "internalTemperature",
+		value: "128"
+	});
 });
