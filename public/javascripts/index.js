81fadf2e7a8aca7876f62a14780eb7c83805ef0f
Sapan Purohit <Sapan_Purohit@ad.infosys.com>
2017-10-06 19:32:58 +0530

    trying grid code12

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,7 +3,24 @@ $(document).ready(function () {
 	//variables declaration
 	var tableData=[], tableDataReverse=[];
 	
-	$("#chart").shieldChart({
+	
+			
+		
+	//websocket connection
+	var ws = new WebSocket('wss://' + location.host);
+	
+	ws.onopen = function () {
+		console.log('Successfully connect WebSocket');
+	}
+	
+	//websocket receiving messages
+	ws.onmessage = function (message) {
+		console.log('Receive message via websocket in client side: ' + message.data);
+		try {
+			var obj = JSON.parse(message.data);
+						
+						
+			$("#chart").shieldChart({
                 theme: "light",
                 exportOptions: {
                     image: false,
@@ -40,115 +57,8 @@ $(document).ready(function () {
                     data: [152, 234, 123, 348]
                 }]
             });
-			
-	$("#grid").shieldGrid({
-            dataSource: {
-				data: tableDataReverse
-			},
-			rowHover: false,			
-            scrolling: {
-                virtual: true
-            },
-            sorting: true,
-            columnReorder: true,
-            columns: [
-                { field: "key", title: "Key" },
-                { field: "value", title: "Value" }
-            ]
-        });
-	
-	//updating table (live)
-    function refreshGird() {
-        var grid = $("#grid").swidget(),
-            initialOptions = grid.initialOptions;
-        initialOptions.dataSource = {
-            data: tableDataReverse
-        };
-        grid.refresh(initialOptions);
-    }
-	
-	//styling table (assigning red/green colour)
-	function gridDataBound(e) {
-		var data = e.target.dataSource.view;
-		var rows = e.target.contentTable.find(">tbody>tr");
-		for (var i = 0; i < data.length; i++) {
-            var item = data[i];
-            if (item.message == thresholdMsg) {
-                $(rows[i].cells[3]).addClass("red");
-            }
-            if (item.message == msg) {
-                $(rows[i].cells[3]).addClass("green");
-            }
-        }
-    }
-		
-	if(tableData.length == 5){
-						for(var i=0; i<(tableData.length)-1; i++){
-							tableData[i]= tableData[i+1];
-						}
-						tableData[4]={
-							key: "internalTemperature",
-							value: "128"
-						};
-					}
-					else{
-						tableData.push({
-							key: "internalTemperature",
-							value: "128"
-						});
-					}	
-					
-		tableDataReverse = tableData.slice(0);
-		tableDataReverse.reverse();
-		
-		
-	//websocket connection
-	var ws = new WebSocket('wss://' + location.host);
-	
-	ws.onopen = function () {
-		console.log('Successfully connect WebSocket');
-	}
-	
-	//websocket receiving messages
-	ws.onmessage = function (message) {
-		console.log('Receive message via websocket in client side: ' + message.data);
-		try {
-			var obj = JSON.parse(message.data);
 						
-						tableData.push({
-							key: "Time of reading",
-							value: obj.timeOfReading
-						});
-						tableData.push({
-							key: "Internal temperature",
-							value: obj.internalTemperature
-						});
-						tableData.push({
-							key: "Daily flow",
-							value: obj.dailyFlow
-						});
-						tableData.push({
-							key: "Daily reverse flow",
-							value: obj.dailyReverseFlow
-						});
-						tableData.push({
-							key: "Peak flow rate",
-							value: obj.peakFlowRate
-						});
-						tableData.push({
-							key: "Peak flow rate time",
-							value: obj.peakFlowRateTime
-						});
-						tableData.push({
-							key: "Event time",
-							value: obj.eventTime
-						});
-						
-					
-			tableDataReverse = tableData.slice(0);
-			tableDataReverse.reverse();
 		
-			refreshGird();
 			}
 		catch (err) {
 		  console.error(err);
