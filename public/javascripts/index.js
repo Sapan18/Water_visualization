123fe17b1c988b449c67236729f12f28a79c1a34
Sapan Purohit <Sapan_Purohit@ad.infosys.com>
2017-10-06 19:03:33 +0530

    trying grid code10

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,46 +3,7 @@ $(document).ready(function () {
 	//variables declaration
 	var tableData=[], tableDataReverse=[];
 	var pressure=[], pressurerev=[];
-	
-	
-	$("#chart").shieldChart({
-                theme: "light",
-                exportOptions: {
-                    image: false,
-                    print: false
-                },
-                primaryHeader: {
-                    text: "Pressure, termperature and vibration statistics over six interval"
-                },
-                zoomMode: "xy",
-                seriesSettings: {
-                    bar: {
-                        barOffset: 0
-                    }
-                },
-                axisX: {
-                    categoricalValues: ["1st", "2nd", "3rd", "4th", "5th", "6th"]
-                },
-                axisY: {
-                    title: {
-                        text: "Value statistics"
-                    }
-                },
-                dataSeries: [{
-                    seriesType: "bar",
-                    collectionAlias: "Pressure",
-                    data: pressurerev
-                }, {
-                    seriesType: "bar",
-                    collectionAlias: "Temperature",
-                    data: [72, 90, 84, 78, 67, 83]
-                }, {
-                    seriesType: "bar",
-                    collectionAlias: "Vibration",
-                    data: [152, 234, 123, 348]
-                }]
-            });
-	
+		
 			
 	$("#grid").shieldGrid({
             dataSource: {
@@ -85,26 +46,7 @@ $(document).ready(function () {
         }
     }
 		
-	if(tableData.length == 5){
-						for(var i=0; i<(tableData.length)-1; i++){
-							tableData[i]= tableData[i+1];
-						}
-						tableData[4]={
-							key: "internalTemperature",
-							value: "128"
-						};
-					}
-					else{
-						tableData.push({
-							key: "internalTemperature",
-							value: "128"
-						});
-					}	
-					
-		tableDataReverse = tableData.slice(0);
-		tableDataReverse.reverse();
-		
-		
+			
 	//websocket connection
 	var ws = new WebSocket('wss://' + location.host);
 	
@@ -178,4 +120,42 @@ $(document).ready(function () {
 		  console.error(err);
 		}
 	}
+	
+	$("#chart").shieldChart({
+                theme: "light",
+                exportOptions: {
+                    image: false,
+                    print: false
+                },
+                primaryHeader: {
+                    text: "Pressure, termperature and vibration statistics over six interval"
+                },
+                zoomMode: "xy",
+                seriesSettings: {
+                    bar: {
+                        barOffset: 0
+                    }
+                },
+                axisX: {
+                    categoricalValues: ["1st", "2nd", "3rd", "4th", "5th", "6th"]
+                },
+                axisY: {
+                    title: {
+                        text: "Value statistics"
+                    }
+                },
+                dataSeries: [{
+                    seriesType: "bar",
+                    collectionAlias: "Pressure",
+                    data: pressurerev
+                }, {
+                    seriesType: "bar",
+                    collectionAlias: "Temperature",
+                    data: [72, 90, 84, 78, 67, 83]
+                }, {
+                    seriesType: "bar",
+                    collectionAlias: "Vibration",
+                    data: [152, 234, 123, 348]
+                }]
+            });
 });
