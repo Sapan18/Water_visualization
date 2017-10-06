3b513081baf64b1deab71f6784935a5e653e65ba
Sapan Purohit <Sapan_Purohit@ad.infosys.com>
2017-10-06 19:22:00 +0530

    trying grid code11

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,8 +2,44 @@ $(document).ready(function () {
 	
 	//variables declaration
 	var tableData=[], tableDataReverse=[];
-	var pressure=[], pressurerev=[];
-		
+	
+	$("#chart").shieldChart({
+                theme: "light",
+                exportOptions: {
+                    image: false,
+                    print: false
+                },
+                primaryHeader: {
+                    text: "Pressure, termperature and vibration statistics over six interval"
+                },
+                zoomMode: "xy",
+                seriesSettings: {
+                    bar: {
+                        barOffset: 0
+                    }
+                },
+                axisX: {
+                    categoricalValues: ["1st", "2nd", "3rd", "4th", "5th", "6th"]
+                },
+                axisY: {
+                    title: {
+                        text: "Value statistics"
+                    }
+                },
+                dataSeries: [{
+                    seriesType: "bar",
+                    collectionAlias: "Pressure",
+                    data: [630, 400, 743, 503, 702, 383]
+                }, {
+                    seriesType: "bar",
+                    collectionAlias: "Temperature",
+                    data: [72, 90, 84, 78, 67, 83]
+                }, {
+                    seriesType: "bar",
+                    collectionAlias: "Vibration",
+                    data: [152, 234, 123, 348]
+                }]
+            });
 			
 	$("#grid").shieldGrid({
             dataSource: {
@@ -46,7 +82,26 @@ $(document).ready(function () {
         }
     }
 		
-			
+	if(tableData.length == 5){
+						for(var i=0; i<(tableData.length)-1; i++){
+							tableData[i]= tableData[i+1];
+						}
+						tableData[4]={
+							key: "internalTemperature",
+							value: "128"
+						};
+					}
+					else{
+						tableData.push({
+							key: "internalTemperature",
+							value: "128"
+						});
+					}	
+					
+		tableDataReverse = tableData.slice(0);
+		tableDataReverse.reverse();
+		
+		
 	//websocket connection
 	var ws = new WebSocket('wss://' + location.host);
 	
@@ -89,28 +144,7 @@ $(document).ready(function () {
 							value: obj.eventTime
 						});
 						
-						pressure.push({
-							parseInt("453")
-						});
-						pressure.push({
-							parseInt("452")
-						});
-						pressure.push({
-							parseInt("451")
-						});
-						pressure.push({
-							parseInt("450")
-						});
-						pressure.push({
-							parseInt("449")
-						});
-						pressure.push({
-							parseInt("448")
-						});
-						
-						
-			pressurerev = pressure.slice(0)	;
-			pressurerev.reverse;
+					
 			tableDataReverse = tableData.slice(0);
 			tableDataReverse.reverse();
 		
@@ -120,42 +154,4 @@ $(document).ready(function () {
 		  console.error(err);
 		}
 	}
-	
-	$("#chart").shieldChart({
-                theme: "light",
-                exportOptions: {
-                    image: false,
-                    print: false
-                },
-                primaryHeader: {
-                    text: "Pressure, termperature and vibration statistics over six interval"
-                },
-                zoomMode: "xy",
-                seriesSettings: {
-                    bar: {
-                        barOffset: 0
-                    }
-                },
-                axisX: {
-                    categoricalValues: ["1st", "2nd", "3rd", "4th", "5th", "6th"]
-                },
-                axisY: {
-                    title: {
-                        text: "Value statistics"
-                    }
-                },
-                dataSeries: [{
-                    seriesType: "bar",
-                    collectionAlias: "Pressure",
-                    data: pressurerev
-                }, {
-                    seriesType: "bar",
-                    collectionAlias: "Temperature",
-                    data: [72, 90, 84, 78, 67, 83]
-                }, {
-                    seriesType: "bar",
-                    collectionAlias: "Vibration",
-                    data: [152, 234, 123, 348]
-                }]
-            });
 });
