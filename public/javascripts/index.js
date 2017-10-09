e53faa994997f89f50ac0774a2dd2adbf52bc4ca
Sapan Purohit <Sapan_Purohit@ad.infosys.com>
2017-10-09 12:03:38 +0530

    code4

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,45 +3,6 @@ $(document).ready(function () {
 	//variables declaration
 	var tableData=[], tableDataReverse=[];
 	
-	//Display empty chart initially
-	$("#chart").shieldChart({
-                theme: "light",
-                exportOptions: {
-                    image: false,
-                    print: false
-                },
-                primaryHeader: {
-                    text: "Pressure, termperature and vibration statistics over six interval"
-                },
-                zoomMode: "xy",
-                seriesSettings: {
-                    bar: {
-                        barOffset: 0
-                    }
-                },
-                axisX: {
-                    categoricalValues: ["1st", "2nd", "3rd", "4th", "5th", "6th"]
-                },
-                axisY: {
-                    title: {
-                        text: "Value statistics"
-                    }
-                },
-                dataSeries: [{
-                    seriesType: "bar",
-                    collectionAlias: "Pressure",
-                    data: [0, 0, 0, 0, 0, 0]
-                }, {
-                    seriesType: "bar",
-                    collectionAlias: "Temperature",
-                    data: [0, 0, 0, 0, 0, 0]
-                }, {
-                    seriesType: "bar",
-                    collectionAlias: "Vibration",
-                    data: [0, 0, 0, 0]
-                }]
-            });
-	
 	$("#grid").shieldGrid({
             dataSource: {
 				data: tableDataReverse
@@ -57,7 +18,18 @@ $(document).ready(function () {
                 { field: "value", title: "Value" }
             ]
         });
-			
+		
+		
+	//updating table (live)
+    function refreshGird() {
+        var grid = $("#grid").swidget(),
+            initialOptions = grid.initialOptions;
+        initialOptions.dataSource = {
+            data: tableDataReverse
+        };
+        grid.refresh(initialOptions);
+    }
+	
 	//styling table (assigning red/green colour)
 	function gridDataBound(e) {
 		var data = e.target.dataSource.view;
@@ -73,95 +45,26 @@ $(document).ready(function () {
         }
     }
 		
+	if(tableData.length == 5){
+						for(var i=0; i<(tableData.length)-1; i++){
+							tableData[i]= tableData[i+1];
+						}
+						tableData[4]={
+							key: "internalTemperature",
+							value: "128"
+						};
+					}
+					else{
+						tableData.push({
+							key: "internalTemperature",
+							value: "128"
+						});
+					}	
+		tableDataReverse = tableData.slice(0);
+		tableDataReverse.reverse();
 		
-	//websocket connection
-	var ws = new WebSocket('wss://' + location.host);
 	
-	ws.onopen = function () {
-		console.log('Successfully connect WebSocket');
-	}
 	
-	//websocket receiving messages
-	ws.onmessage = function (message) {
-		console.log('Receive message via websocket in client side: ' + message.data);
-		try {
-			var obj = JSON.parse(message.data);
-			
-			$("#chart").shieldChart({
-                theme: "light",
-                exportOptions: {
-                    image: false,
-                    print: false
-                },
-                primaryHeader: {
-                    text: "Pressure, termperature and vibration statistics over six interval"
-                },
-                zoomMode: "xy",
-                seriesSettings: {
-                    bar: {
-                        barOffset: 0
-                    }
-                },
-                axisX: {
-                    categoricalValues: ["1st", "2nd", "3rd", "4th", "5th", "6th"]
-                },
-                axisY: {
-                    title: {
-                        text: "Value statistics"
-                    }
-                },
-                dataSeries: [{
-                    seriesType: "bar",
-                    collectionAlias: "Pressure",
-                    data: [parseInt(obj.pressure[0].Value), parseInt(obj.pressure[1].Value), parseInt(obj.pressure[2].Value), parseInt(obj.pressure[3].Value), parseInt(obj.pressure[4].Value), parseInt(obj.pressure[5].Value)]
-                }, {
-                    seriesType: "bar",
-                    collectionAlias: "Temperature",
-                    data: [parseInt(obj.temp[0].Value), parseInt(obj.temp[1].Value), parseInt(obj.temp[2].Value), parseInt(obj.temp[3].Value), parseInt(obj.temp[4].Value), parseInt(obj.temp[5].Value)]
-                }, {
-                    seriesType: "bar",
-                    collectionAlias: "Vibration",
-                    data: [parseInt(obj.vibration[0].Value), parseInt(obj.vibration[1].Value), parseInt(obj.vibration[2].Value), parseInt(obj.vibration[3].Value)]
-                }]
-            });
-						
-						tableData.push({
-							key: "Time of reading",
-							value: obj.timeOfReading
-						});
-						tableData.push({
-							key: "Internal temperature",
-							value: obj.internalTemperature
-						});
-						tableData.push({
-							key: "Daily flow",
-							value: obj.dailyFlow
-						});
-						tableData.push({
-							key: "Daily reverse flow",
-							value: obj.dailyReverseFlow
-						});
-						tableData.push({
-							key: "Peak flow rate",
-							value: obj.peakFlowRate
-						});
-						tableData.push({
-							key: "Peak flow rate time",
-							value: obj.peakFlowRateTime
-						});
-						tableData.push({
-							key: "Event time",
-							value: obj.eventTime
-						});
-						
-					
-			tableDataReverse = tableData.slice(0);
-			tableDataReverse.reverse();
-		
-			//refreshGird();
-			}
-		catch (err) {
-		  console.error(err);
-		}
-	}
+	
+	
 });
