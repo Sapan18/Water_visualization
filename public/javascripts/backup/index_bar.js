fd9beb7fc6a6a0c3f8f64fa5f696867a4bd3741c
Sapan Purohit <Sapan_Purohit@ad.infosys.com>
2017-10-09 16:10:05 +0530

    all three2

---
+++ public/javascripts/backup/index_bar.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+	
+	$("#chart").shieldChart({
+                theme: "light",
+                exportOptions: {
+                    image: false,
+                    print: false
+                },
+                primaryHeader: {
+                    text: "Pressure, termperature and vibration statistics over six interval"
+                },
+                zoomMode: "xy",
+                seriesSettings: {
+                    bar: {
+                        barOffset: 0
+                    }
+                },
+                axisX: {
+                    categoricalValues: ["1st", "2nd", "3rd", "4th", "5th", "6th"]
+                },
+                axisY: {
+                    title: {
+                        text: "Value statistics"
+                    }
+                },
+                dataSeries: [{
+                    seriesType: "bar",
+                    collectionAlias: "Pressure",
+                    data: [630, 400, 743, 503, 702, 383]
+                }, {
+                    seriesType: "bar",
+                    collectionAlias: "Temperature",
+                    data: [72, 90, 84, 78, 67, 83]
+                }, {
+                    seriesType: "bar",
+                    collectionAlias: "Vibration",
+                    data: [152, 234, 123, 348]
+                }]
+            });
+});
--- public/javascripts/backup/index_bar_staticTable_line.js
+++ public/javascripts/backup/index_bar_staticTable_line.js
@@ -81,7 +81,7 @@ $(document).ready(function () {
             dataSeries: [{
                 seriesType: 'area',
                 collectionAlias: "Budget in Thousands",
-                data: []
+                data: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48]
             }]
         });
 	
@@ -180,33 +180,6 @@ $(document).ready(function () {
 							key: "Event time",
 							value: obj.eventTime
 						});
-						
-			//Axis marker for interval flow   
-			$("#line").shieldChart({
-            theme: "light",
-            primaryHeader: {
-                text: "Interval Flow"
-            },
-            exportOptions: {
-                image: false,
-                print: false
-            },
-            axisX: {
-                categoricalValues: []
-            },
-            tooltipSettings: {
-                chartBound: true,
-                axisMarkers: {
-                    enabled: true,
-                    mode: 'xy'
-                }                    
-            },
-            dataSeries: [{
-                seriesType: 'area',
-                collectionAlias: "Budget in Thousands",
-                data: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48]
-            }]
-        });
 						
 					
 			tableDataReverse = tableData.slice(0);
