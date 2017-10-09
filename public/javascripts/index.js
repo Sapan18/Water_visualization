15307b26aa50247c9c78b7ce907a0b410236303d
Sapan Purohit <Sapan_Purohit@ad.infosys.com>
2017-10-09 16:57:34 +0530

    grid

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -53,8 +53,8 @@ $(document).ready(function () {
             sorting: true,
             columnReorder: true,
             columns: [
-                { field: "key", title: "Key" attributes: {style: "text-align: center; font-size: 14px"}, headerAttributes: { style: "text-align: center; font-size: 16px"}},
-                { field: "value", title: "Value" attributes: {style: "text-align: center; font-size: 14px"}, headerAttributes: { style: "text-align: center; font-size: 16px"}}
+                { field: "key", title: "Key", attributes: {style: "text-align: center; font-size: 14px"}, headerAttributes: { style: "text-align: center; font-size: 16px"}},
+                { field: "value", title: "Value", attributes: {style: "text-align: center; font-size: 14px; padding-Top:10px"}, headerAttributes: { style: "text-align: center; font-size: 16px"}}
             ]
         });
 			
