2426b9892799c0d3128c4b3da640158523f75878
Sapan Purohit <Sapan_Purohit@ad.infosys.com>
2017-10-09 14:23:32 +0530

    code2

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -53,8 +53,8 @@ $(document).ready(function () {
             sorting: true,
             columnReorder: true,
             columns: [
-                { field: "key", title: "Key" },
-                { field: "value", title: "Value" }
+                { field: "key", title: "Key", attributes: {style: "text-align: center; font-size: 14px"}, headerAttributes: { style: "text-align: center; font-size: 16px"}},
+                { field: "value", title: "Value", attributes: {style: "text-align: center; font-size: 14px"}, headerAttributes: { style: "text-align: center; font-size: 16px"}}
             ]
 			});	
 			
