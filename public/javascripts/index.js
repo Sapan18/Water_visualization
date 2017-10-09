41b1ada77ccf5e7e1e9db0bf0440c2bd8153c4fc
Sapan Purohit <Sapan_Purohit@ad.infosys.com>
2017-10-09 20:25:03 +0530

    live2

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -56,32 +56,7 @@ $(document).ready(function () {
                 { field: "value", title: "Value", attributes: {style: "text-align: center; font-size: 14px"}, headerAttributes: { style: "text-align: center; font-size: 16px"}}]
         });
         
-    //Axis marker for interval flow   
-	$("#line").shieldChart({
-            theme: "light",
-            primaryHeader: {
-                text: "Interval Flow"
-            },
-            exportOptions: {
-                image: false,
-                print: false
-            },
-            axisX: {
-                categoricalValues: []
-            },
-            tooltipSettings: {
-                chartBound: true,
-                axisMarkers: {
-                    enabled: true,
-                    mode: 'xy'
-                }                    
-            },
-            dataSeries: [{
-                seriesType: 'area',
-                collectionAlias: "Budget in Thousands",
-                data: []
-            }]
-        });
+    
 		
 	//updating table (live)
     function refreshGird() {
@@ -235,34 +210,7 @@ $(document).ready(function () {
 							key: "Event time",
 							value: obj.eventTime
 						});
-						
-			//Axis marker for interval flow   
-			$("#line").shieldChart({
-            theme: "light",
-            primaryHeader: {
-                text: "Interval Flow"
-            },
-            exportOptions: {
-                image: false,
-                print: false
-            },
-            axisX: {
-                categoricalValues: []
-            },
-            tooltipSettings: {
-                chartBound: true,
-                axisMarkers: {
-                    enabled: true,
-                    mode: 'xy'
-                }                    
-            },
-            dataSeries: [{
-                seriesType: 'area',
-                collectionAlias: "Budget in Thousands",
-                data: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48]
-            }]
-        });
-						
+					
 								
 			tableDataReverse = tableData.slice(0);
 		
